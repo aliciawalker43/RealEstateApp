562fc8bf04a2854a9059cc1736a11c78cd0c22cf
Alicia Walker <aliciawalker@Alicias-MacBook-Pro.local>
2025-12-08 11:13:00 -0500

    first upgrade attempt

--- src/main/java/RealEstateApp/Pojo/User.java
+++ src/main/java/RealEstateApp/Pojo/User.java
@@ -2,12 +2,12 @@ package RealEstateApp.Pojo;
 
 
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Entity;
 
 
 import org.springframework.beans.factory.annotation.Value;
